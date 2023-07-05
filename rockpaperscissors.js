ce4dbf237165e4f091f7701955e1787fefdd4526
woofda <130379195+woofda@users.noreply.github.com>
2023-07-04 21:52:00 -0500

    Remove unnecessary code and add comments

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -4,6 +4,8 @@
 // Randomly selects rock, paper, or scissors for computer
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
+
+    // Selections are returned as strings
     switch (choice) {
         case 0:
             return "rock";
@@ -16,13 +18,19 @@ function getComputerChoice() {
     }
 }
 
+// Takes player and computer selections as strings and
+// returns the winner as 1 - player, 0 - draw, -1 - computer
 function playRound(playerSelection, computerSelection) {
+
+    // Format player input to lowercase
     let playerLower = playerSelection.toLowerCase();
+
+    // Draw check
     if (playerLower === computerSelection) {
         return 0;
     }
 
-    let playerWon = false;
+    // Player win checks
     if (playerLower === 'rock' && computerSelection === 'scissors') {
         return 1;
     }
@@ -41,11 +49,14 @@ function game() {
     let playerWins = 0;
     let computerWins = 0;
 
+    // Best of 5 match loop
     while (!gameOver) {
         let computerChoice = getComputerChoice();
         let validInput = false;
         let playerChoice;
 
+        // Basic player input validation; only accepts
+        // 'rock' 'paper' and 'scissors' (non caps sensitive)
         while(!validInput){
             playerChoice = prompt("Enter selection: ");
             if (playerChoice.toLowerCase() === 'rock' || playerChoice.toLowerCase() === 'paper' || playerChoice.toLowerCase() === 'scissors') {
@@ -53,8 +64,10 @@ function game() {
             }
         }
 
+        // Plays single round and returns outcome
         let outcome = playRound(playerChoice, computerChoice);
 
+        // Prints output and adds to win totals
         switch (outcome) {
             case 1:
                 console.log(`You win! ${playerChoice.slice(0,1).toUpperCase() + playerChoice.slice(1).toLowerCase()} beats ${computerChoice}!`)
@@ -71,6 +84,7 @@ function game() {
                 console.warn("Outcome error");
         }
 
+        // Check for best of 5 match completion
         if (playerWins >= 3) {
             console.log("Player wins the best of 5!");
             gameOver = true;
