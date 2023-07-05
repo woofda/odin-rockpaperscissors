3c57104a795e166dc55711603252668f8e6fa730
woofda <130379195+woofda@users.noreply.github.com>
2023-07-04 21:17:10 -0500

    Add play round function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -15,3 +15,30 @@ function getComputerChoice() {
             console.warn("Computer choice error");
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerLower = playerSelection.toLowerCase();
+    if (playerLower === computerSelection) {
+        return "It's a draw!";
+    }
+
+    let playerWon = false;
+    if (playerLower === 'rock' && computerSelection === 'scissors') {
+        playerWon = true;
+    }
+    else if (playerLower === 'paper' && computerSelection === 'rock') {
+        playerWon = true;
+    }
+    else if (playerLower === 'scissors' && computerSelection === 'paper') {
+        playerWon = true;
+    }
+
+    if (playerWon) {
+        playerLower = playerLower.slice(0, 1).toUpperCase() + playerLower.slice(1);
+        return `You win! ${playerLower} beats ${computerSelection}!`
+    }
+    else {
+        let computerFormatted = computerSelection.slice(0, 1).toUpperCase() + computerSelection.slice(1);
+        return `You lose! ${computerFormatted} beats ${playerLower}!`
+    }
+}
