2502486349f9577a4a6fea73a0f559f7a2deb0c1
woofda <130379195+woofda@users.noreply.github.com>
2023-07-16 22:54:40 -0500

    Complete styling and animations

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -3,12 +3,31 @@
 
 // Global variables
 let gameOver = false;
+let roundPlaying = false;
 let playerWins = 0;
 let computerWins = 0;
 
 // Randomly selects rock, paper, or scissors for computer
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
+
+    for(button of compButtons){
+        button.style.color = 'whitesmoke';
+    }
+
+    setTimeout(() => {
+        switch (choice) {
+            case 0:
+                compButtons[0].style.color = '#F4D160';
+                break;
+            case 1:
+                compButtons[1].style.color = '#F4D160';
+                break;
+            case 2:
+                compButtons[2].style.color = '#F4D160';
+                break;
+        }
+    }, 750);
 
     // Selections are returned as strings
     switch (choice) {
@@ -56,13 +75,20 @@ function resetGame() {
     computerWins = 0;
 
     document.querySelector('.reset').remove();
-    document.querySelector('.player').innerText = playerWins;
-    document.querySelector('.computer').innerText = computerWins;
+    document.querySelector('.player-score').innerText = playerWins;
+    document.querySelector('.computer-score').innerText = computerWins;
     document.querySelector('#outcome-text').innerText = "";
 
-    for(button of buttons) {
-        button.classList.add("active");
+    for(item of document.querySelectorAll('.colorable')) {
+        item.style.color = 'whitesmoke';
     }
+
+    for(button of buttons) {
+        button.classList.add('active');
+    }
+
+    document.querySelector('.player-side').style.borderColor = 'whitesmoke';
+    document.querySelector('.computer-side').style.borderColor = 'whitesmoke';
 }
 
 // Shows reset button on game completion
@@ -74,7 +100,7 @@ function showReset() {
     resetButton.addEventListener('click', () => resetGame());
     resetButton.textContent = "New game";
     resetButton.classList.add('reset');
-    document.querySelector('body').appendChild(resetButton);
+    document.querySelector('main').appendChild(resetButton);
 }
 
 // Results elements
@@ -84,46 +110,83 @@ const computerScore = document.querySelector('.computer');
 
 // Results updater
 function updateResults(result) {
-    if(!gameOver) {
+    let outcome = document.querySelector('#outcome-text');
+    let playerSide = document.querySelector('.player-side');
+    let computerSide = document.querySelector('.computer-side');
+
+    outcome.style.opacity = "0";
+    outcome.innerText = '';
+    playerSide.style.borderColor = 'whitesmoke';
+    computerSide.style.borderColor = 'whitesmoke';
+
+    setTimeout(() => {
         switch (result) {
             case 1:
                 playerWins++;
-                
+                playerSide.style.borderColor = '#F4D160';
                 if(playerWins >= 5) {
                     gameOver = true;
-                    document.querySelector('#outcome-text').innerText = 'Player wins the game!';
+                    outcome.innerText = 'Player wins the game!';
+                    outcome.style.color = '#F4D160';
+                    document.querySelector('.player').style.color = '#F4D160';
                     showReset();
                 }
-                else document.querySelector('#outcome-text').innerText = 'Player wins the round';
+                else outcome.innerText = 'Player wins the round';
                 break;
             case 0:
-                document.querySelector('#outcome-text').innerText = "It's a draw";
+                outcome.innerText = "It's a draw";
                 break;
             case -1:
                 computerWins++;
+                computerSide.style.borderColor = '#F4D160';
                 if(computerWins >= 5) {
                     gameOver = true;
-                    document.querySelector('#outcome-text').innerText = 'Computer wins the game!';
+                    outcome.innerText = 'Computer wins the game!';
+                    outcome.style.color = '#F4D160';
+                    document.querySelector('.computer').style.color = '#F4D160';
                     showReset();
                 }
-                else document.querySelector('#outcome-text').innerText = 'Computer wins the round';
+                else outcome.innerText = 'Computer wins the round';
                 break;
             default:
                 console.warn("Outcome error");
         }
 
-        document.querySelector('.player').innerText = playerWins;
-        document.querySelector('.computer').innerText = computerWins;
-    }
-
+        document.querySelector('.player-score').innerText = playerWins;
+        document.querySelector('.computer-score').innerText = computerWins;
+        document.querySelector('#outcome-text').style.opacity = "100";
+        roundPlaying = false;
+    }, 1500);
 }
 
 // Button listeners
 const buttons = document.querySelectorAll("button");
+const compButtons = document.querySelectorAll("div.choice-button");
 
 // Buttons trigger playRound with the appropriate choice
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        updateResults(playRound(button.id, getComputerChoice()));
+        if(!gameOver){
+            if(!roundPlaying){
+                roundPlaying = true;
+                switch(button.id) {
+                    case "rock":
+                        buttons[0].style.color = '#F4D160';
+                        buttons[1].style.color = 'whitesmoke';
+                        buttons[2].style.color = 'whitesmoke';
+                        break;
+                    case "paper":
+                        buttons[0].style.color = 'whitesmoke';
+                        buttons[1].style.color = '#F4D160';
+                        buttons[2].style.color = 'whitesmoke';
+                        break;
+                    case "scissors":
+                        buttons[0].style.color = 'whitesmoke';
+                        buttons[1].style.color = "whitesmoke";
+                        buttons[2].style.color = '#F4D160';
+                }
+                updateResults(playRound(button.id, getComputerChoice()));
+            }
+        }
     });
 })
